4dd34e42ce6237fc2a93eae6238104f8555499cf
DineshReddyKommera <dineshreddykommera@gmail.com>
2020-06-20 15:32:52 -0700

    Modified layouts for both tablets and phones

--- main/java/com/example/vehicledata/VehicleDetailsActivity.java
+++ main/java/com/example/vehicledata/VehicleDetailsActivity.java
@@ -1,9 +1,11 @@
 package com.example.vehicledata;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableStringBuilder;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,9 +13,9 @@ import com.example.vehicledata.content.VehicleDetailInformation;
 import com.squareup.picasso.Picasso;
 
 /**
- *  A Simple {@link android.app.Activity} subclass
- *  Based on Intent obtained from {@link MainActivity}
- *  we de-serialize object and render data to respective fields.
+ * A Simple {@link android.app.Activity} subclass
+ * Based on Intent obtained from {@link MainActivity}
+ * we de-serialize object and render data to respective fields.
  */
 public class VehicleDetailsActivity extends AppCompatActivity {
     TextView makeModel;
@@ -21,9 +23,10 @@ public class VehicleDetailsActivity extends AppCompatActivity {
     TextView description;
     TextView updatedDate;
     ImageView vehicleImage;
-    String SAVED_OBJECT="savedObject";
-    SpannableStringBuilder vehicleDescriptionFormatter;
+    String SAVED_OBJECT = "savedObject";
+    SpannableString vehicleFormatter;
     String vehicleDescriptionLabel;
+    String vehicleLastUpdateLabel;
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -35,40 +38,50 @@ public class VehicleDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle_details);
-        description=findViewById(R.id.m_txt_vehicle_details_description);
-        makeModel=findViewById(R.id.m_txt_vehicle_details_model);
-        price=findViewById(R.id.m_txt_vehicle_details_price);
-        updatedDate=findViewById(R.id.m_txt_vehicle_details_updated_date);
-        vehicleImage= findViewById(R.id.m_vehicle_details_image);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        description = findViewById(R.id.m_txt_vehicle_details_description);
+        makeModel = findViewById(R.id.m_txt_vehicle_details_model);
+        price = findViewById(R.id.m_txt_vehicle_details_price);
+        updatedDate = findViewById(R.id.m_txt_vehicle_details_updated_date);
+        vehicleImage = findViewById(R.id.m_vehicle_details_image);
         VehicleDetailInformation vehicleDetailInformation;
         if (savedInstanceState == null) {
             vehicleDetailInformation =
                     (VehicleDetailInformation) getIntent().getSerializableExtra("VEHICLE_INFO");
-        }
-        else{
-            vehicleDetailInformation=(VehicleDetailInformation)savedInstanceState.getSerializable(SAVED_OBJECT);
+        } else {
+            vehicleDetailInformation = (VehicleDetailInformation) savedInstanceState.getSerializable(SAVED_OBJECT);
         }
 
-        vehicleDescriptionLabel=getString(R.string.vehicle_description_label);
-        vehicleDescriptionFormatter = new SpannableStringBuilder(vehicleDescriptionLabel+vehicleDetailInformation.getVehicleDesc());
-        vehicleDescriptionFormatter.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD),
-                0, vehicleDescriptionLabel.length() , Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        description.setText(vehicleDescriptionFormatter);
+        vehicleDescriptionLabel = getString(R.string.vehicle_description_label);
+        vehicleFormatter = new SpannableString(vehicleDescriptionLabel + vehicleDetailInformation.getVehicleDesc());
+        vehicleFormatter.setSpan(new RelativeSizeSpan(1.6f), 0, 21, 0); // set size
+        vehicleFormatter.setSpan(new ForegroundColorSpan(Color.parseColor("#008b8b")), 0, 21, 0);// set color
+        description.setText(vehicleFormatter);
 
         price.setText(vehicleDetailInformation.getPrice());
-        makeModel.setText(vehicleDetailInformation.getmMake()+" - "+vehicleDetailInformation.getmModel());
-        updatedDate.setText("Last update: "+vehicleDetailInformation.getUpdateDate());
+        makeModel.setText(vehicleDetailInformation.getmMake() + " - " + vehicleDetailInformation.getmModel());
+        vehicleLastUpdateLabel = getString(R.string.vehicle_updatedate_label);
+        vehicleFormatter = new SpannableString(vehicleLastUpdateLabel + vehicleDetailInformation.getUpdateDate());
+        vehicleFormatter.setSpan(new RelativeSizeSpan(1f), 0, 12, 0); // set size
+        vehicleFormatter.setSpan(new ForegroundColorSpan(Color.parseColor("#008b8b")), 0, 12, 0);// set color
+        updatedDate.setText(vehicleFormatter);
 
-        if(vehicleDetailInformation.getImageUrl()!=null
-                && vehicleDetailInformation.getImageUrl().length()!=0) {
+        if (vehicleDetailInformation.getImageUrl() != null
+                && vehicleDetailInformation.getImageUrl().length() != 0) {
             Picasso.get().load(vehicleDetailInformation.getImageUrl())    //Desired source of Image
                     .placeholder(R.drawable.loading_image)                  //This acts as placeholder until image is fetched
                     .error(R.drawable.image_clip)                           //When Application failed to load image, this image is displayed
                     .into(vehicleImage);
-        }else {
+        } else {
             Picasso.get().load(R.drawable.image_clip)                   //Loading Car clip since Source URL is NULL
                     .placeholder(R.drawable.loading_image)                  //This acts as placeholder until image is fetched
                     .into(vehicleImage);
         }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
--- main/java/com/example/vehicledata/VehicleDetailsFragment.java
+++ main/java/com/example/vehicledata/VehicleDetailsFragment.java
@@ -1,9 +1,13 @@
 package com.example.vehicledata;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Spannable;
+import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +18,7 @@ import com.example.vehicledata.content.VehicleDetailInformation;
 import com.squareup.picasso.Picasso;
 
 // TODO: Customize data based on the data fetched
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link VehicleDetailsFragment#newInstance} factory method to
@@ -30,8 +35,10 @@ public class VehicleDetailsFragment extends Fragment {
     TextView description;
     TextView updatedDate;
     ImageView vehicleImage;
-    SpannableStringBuilder vehicleDescriptionFormatter;
+    SpannableString vehicleFormatter;
     String vehicleDescriptionLabel;
+    String vehicleLastUpdateLabel;
+
     public VehicleDetailsFragment() {
         // Required empty public constructor
     }
@@ -46,7 +53,7 @@ public class VehicleDetailsFragment extends Fragment {
     public static VehicleDetailsFragment newInstance(VehicleDetailInformation vehicleDetailInformation) {
         VehicleDetailsFragment fragment = new VehicleDetailsFragment();
         Bundle args = new Bundle();
-        args.putSerializable(VEHICLE_ID,vehicleDetailInformation);
+        args.putSerializable(VEHICLE_ID, vehicleDetailInformation);
         fragment.setArguments(args);
         return fragment;
     }
@@ -64,31 +71,35 @@ public class VehicleDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView= inflater.inflate(R.layout.vehicle_details, container, false);
+        View rootView = inflater.inflate(R.layout.vehicle_details, container, false);
 
         if (mVehicleDetailInformation != null) {
-            description=rootView.findViewById(R.id.m_txt_vehicle_details_description);
-            makeModel=rootView.findViewById(R.id.m_txt_vehicle_details_model);
-            price=rootView.findViewById(R.id.m_txt_vehicle_details_price);
-            updatedDate=rootView.findViewById(R.id.m_txt_vehicle_details_updated_date);
-            vehicleImage= rootView.findViewById(R.id.m_vehicle_details_image);
+            description = rootView.findViewById(R.id.m_txt_vehicle_details_description);
+            makeModel = rootView.findViewById(R.id.m_txt_vehicle_details_model);
+            price = rootView.findViewById(R.id.m_txt_vehicle_details_price);
+            updatedDate = rootView.findViewById(R.id.m_txt_vehicle_details_updated_date);
+            vehicleImage = rootView.findViewById(R.id.m_vehicle_details_image);
 
-            vehicleDescriptionLabel=getString(R.string.vehicle_description_label);
-            vehicleDescriptionFormatter = new SpannableStringBuilder(vehicleDescriptionLabel+mVehicleDetailInformation.getVehicleDesc());
-            vehicleDescriptionFormatter.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD),
-                    0, vehicleDescriptionLabel.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            description.setText(vehicleDescriptionFormatter);
+            vehicleDescriptionLabel = getString(R.string.vehicle_description_label);
+            vehicleFormatter = new SpannableString(vehicleDescriptionLabel + mVehicleDetailInformation.getVehicleDesc());
+            vehicleFormatter.setSpan(new RelativeSizeSpan(1.2f), 0, 21, 0); // set size
+            vehicleFormatter.setSpan(new ForegroundColorSpan(Color.parseColor("#008b8b")), 0, 21, 0);// set color
+            description.setText(vehicleFormatter);
 
             price.setText(mVehicleDetailInformation.getPrice());
-            makeModel.setText(mVehicleDetailInformation.getmMake()+ " - "+ mVehicleDetailInformation.getmModel());
-            updatedDate.setText("Last update: "+mVehicleDetailInformation.getUpdateDate());
-            if(mVehicleDetailInformation.getImageUrl()!=null
-                    && mVehicleDetailInformation.getImageUrl().length()!=0) {
+            makeModel.setText(mVehicleDetailInformation.getmMake() + " - " + mVehicleDetailInformation.getmModel());
+            vehicleLastUpdateLabel = getString(R.string.vehicle_updatedate_label);
+            vehicleFormatter = new SpannableString(vehicleLastUpdateLabel + mVehicleDetailInformation.getUpdateDate());
+            vehicleFormatter.setSpan(new RelativeSizeSpan(1f), 0, 12, 0); // set size
+            vehicleFormatter.setSpan(new ForegroundColorSpan(Color.parseColor("#008b8b")), 0, 12, 0);// set color
+            updatedDate.setText(vehicleFormatter);
+            if (mVehicleDetailInformation.getImageUrl() != null
+                    && mVehicleDetailInformation.getImageUrl().length() != 0) {
                 Picasso.get().load(mVehicleDetailInformation.getImageUrl())     //Desired source of Image
                         .placeholder(R.drawable.loading_image)                  //This acts as placeholder until image is fetched
                         .error(R.drawable.image_clip)                           //When Application failed to load image, this image is displayed
                         .into(vehicleImage);                                    //Target View where we would like to place Image
-            }else {
+            } else {
                 Picasso.get().load(R.drawable.image_clip)                       //Loading Car clip since Source URL is NULL
                         .placeholder(R.drawable.loading_image)                  //This acts as placeholder until image is fetched
                         .into(vehicleImage);
